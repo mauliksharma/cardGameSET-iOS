a786bd5411b69ec88124aa417a06d2ff16715b62
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-09-28 08:46:21 +0530

    matching functional

---
+++ cardGameSET/Game.swift
@@ -0,0 +1,141 @@
+//
+//  Game.swift
+//  cardGameSET
+//
+//  Created by Maulik Sharma on 28/09/18.
+//  Copyright © 2018 Geekskool. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var sortedCardsCollection = [Card]()
+    var randomCardsCollection = [Card]()
+    
+    init() {
+        for sym in 0...2 {
+            for num in 0...2 {
+                for col in 0...2 {
+                    for sha in 0...2 {
+                        let newCard = Card(symbol: sym, number: num, color: col, shade: sha, hashValue: Card.getUniqueHash())
+                        sortedCardsCollection.append(newCard)
+                    }
+                }
+            }
+        }
+        
+        for _ in 0..<sortedCardsCollection.count {
+            let randomIndex = Int(arc4random_uniform(UInt32(sortedCardsCollection.count)))
+            randomCardsCollection.append(sortedCardsCollection.remove(at: randomIndex))
+        }
+        
+        for index in 0...11 {
+            loadNewCard(at: index)
+        }
+    }
+    
+    var loadedCardsDict = [Int: Card]()
+    var keysWhereCardsNA = [Int]()
+    var lastCardLoadedKey : Int = 11    //12 cards loaded with keys 0-11 on game creation
+    
+    var selectedCards = [Card]()
+    var matchedCards = [Card]()
+    
+    var symbolSet = Set<Int>()
+    var numberSet = Set<Int>()
+    var colorSet = Set<Int>()
+    var shadeSet = Set<Int>()
+    
+    func clearSets() {
+        symbolSet.removeAll()
+        numberSet.removeAll()
+        colorSet.removeAll()
+        shadeSet.removeAll()
+    }
+    
+    func loadNewCard(at index: Int) {
+        if !randomCardsCollection.isEmpty {
+            loadedCardsDict[index] = randomCardsCollection.remove(at: 0)
+        }
+        else { //in case of no cards available for replacement
+            keysWhereCardsNA.append(index)
+            loadedCardsDict[index] = nil
+        }
+    }
+    
+    func replaceCards() {
+        for card in matchedCards {
+            if let key = loadedCardsDict.keys(forValue: card) { //refer extension below
+                loadNewCard(at: key)
+            }
+        }
+    }
+    
+    
+    func deal3NewCards() {
+        if !matchedCards.isEmpty {
+            replaceCards()
+            matchedCards.removeAll()
+        }
+        else {
+            if lastCardLoadedKey < 24 { //should not exceed 24 cards at a time
+                for offset in 1...3 {
+                    loadNewCard(at: lastCardLoadedKey + offset)
+                }
+                lastCardLoadedKey += 3
+            }
+        }
+    }
+    
+    func chooseCard(at key: Int) {
+        if let cardToSelect = loadedCardsDict[key] {
+            if !matchedCards.contains(cardToSelect) || !keysWhereCardsNA.contains(key){
+                if !selectedCards.contains(cardToSelect) {  //selecting :-
+                    if selectedCards.count == 2 {   //selecting 3rd card and testing for match
+                        selectedCards.append(cardToSelect)
+                        for cardToMatch in selectedCards {
+                            symbolSet.insert(cardToMatch.symbol)
+                            numberSet.insert(cardToMatch.number)
+                            colorSet.insert(cardToMatch.color)
+                            shadeSet.insert(cardToMatch.shade)
+                        }
+                        if (symbolSet.count != 2) && (numberSet.count != 2) && (colorSet.count != 2) && (shadeSet.count != 2) {
+                            for card in selectedCards {
+                                matchedCards.append(card)
+                            }
+                        }
+                        clearSets()
+                    }
+                    else if selectedCards.count == 3 {  //selecting fresh card after 3 cards tested for match
+                        if !matchedCards.isEmpty {
+                            replaceCards()
+                            matchedCards.removeAll()
+                        }
+                        selectedCards = [cardToSelect]
+                        
+                    }
+                    else {  //if no card or 1 card is selected
+                        selectedCards.append(cardToSelect)
+                    }
+                }
+                else {  //deselecting :-
+                    if selectedCards.count < 3 {
+                        selectedCards = selectedCards.filter(){$0 != cardToSelect}
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+}
+
+extension Dictionary where Value : Equatable {
+    public func keys(forValue value: Value) -> Key? {
+        return keys.filter { self[$0] == value }.first
+    }
+}
+
+
---
+++ cardGameSET/ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  cardGameSET
+//
+//  Created by Maulik Sharma on 27/09/18.
+//  Copyright © 2018 Geekskool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]! {
+        didSet {
+            updateViewFromModel()
+        }
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardButtonIndex =  cardButtons.index(of: sender) {
+            game.chooseCard(at: cardButtonIndex)
+            updateViewFromModel()
+        }
+    }
+    
+    @IBAction func dealMoreCards(_ sender: UIButton) {
+        game.deal3NewCards()
+        updateViewFromModel()
+    }
+    
+    
+    @IBAction func startAgain(_ sender: UIButton) {
+        game = Game()
+        updateViewFromModel()
+    }
+    
+    var game = Game()
+    
+    enum Feature {
+        case symbol(String)
+        case number(Int)
+        case color(UIColor)
+        case shade(Double?, CGFloat?)
+    }
+    
+    var FeatureSpec: [String: Feature] = [
+        "Sym0" : .symbol("●"),
+        "Sym1" : .symbol("■"),
+        "Sym2" : .symbol("▲"),
+        "Num0" : .number(1),
+        "Num1" : .number(2),
+        "Num2" : .number(3),
+        "Col0" : .color(.orange),
+        "Col1" : .color(.blue),
+        "Col2" : .color(.darkGray),
+        "Sha0" : .shade(8.0, nil),
+        "Sha1" : .shade(nil, 1.0),
+        "Sha2" : .shade(nil, 0.4)
+    ]
+    
+    func loadCardTitle(_ card: Card) -> NSAttributedString {
+        var string = String()
+        var attributes = [NSAttributedString.Key : Any]()
+        var tempColorStorage: UIColor?
+        
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 0.7
+        
+        func setFeatureValue(_ key: String) {
+            if let value = FeatureSpec[key] {
+                switch value {
+                case .symbol(let symbol):
+                    string = symbol
+                case .number(let number):
+                    string = Array(repeating: string, count: number).joined(separator: "\n")
+                case .color(let color):
+                    attributes[.foregroundColor] = color
+                    tempColorStorage = color
+                case .shade(let stroke, let alpha):
+                    if stroke != nil {
+                        attributes[.strokeWidth] = stroke
+                    }
+                    if alpha != nil {
+                        if let color = tempColorStorage {
+                            attributes[.foregroundColor] = color.withAlphaComponent(alpha!)
+                        }
+                    }
+                }
+            }
+        }
+        
+        setFeatureValue("Sym\(card.symbol)")
+        setFeatureValue("Num\(card.number)")
+        setFeatureValue("Col\(card.color)")
+        setFeatureValue("Sha\(card.shade)")
+        
+        return NSAttributedString(string: string, attributes: attributes)
+    }
+    
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            button.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+            if let card = game.loadedCardsDict[index] {
+                button.setAttributedTitle(loadCardTitle(card), for: UIControl.State.normal)
+                button.layer.opacity = 1.0
+                if game.matchedCards.contains(card) {
+                    button.layer.borderWidth = 3.0
+                    button.layer.borderColor = UIColor.red.cgColor
+                }
+                else if game.selectedCards.contains(card) {
+                    button.layer.borderWidth = 3.0
+                    button.layer.borderColor = UIColor.blue.cgColor
+                }
+                else {
+                    button.layer.borderWidth = 0
+                    button.layer.borderColor = nil
+                }
+            }
+            else if game.keysWhereCardsNA.contains(index) {
+                button.setAttributedTitle(nil, for: UIControl.State.normal)
+                button.layer.borderWidth = 0
+                button.layer.borderColor = nil
+                button.layer.opacity = 0.0
+            }
+            else {
+                button.setAttributedTitle(nil, for: UIControl.State.normal)
+                button.layer.borderWidth = 0
+                button.layer.borderColor = nil
+                button.layer.opacity = 0.0
+            }
+        }
+    }
+    
+}
